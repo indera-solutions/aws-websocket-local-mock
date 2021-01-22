6fcff24ae1a414485954599b886d0cd5bb6c2122
Stavros <stavros.z@hotmail.com>
2021-01-22 19:24:35 +0200

    working ws

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,11 @@
 import {NestFactory} from '@nestjs/core';
 import {AppModule} from './app.module';
-import WildcardsIoAdapter from "./adapter";
+import {WsAdapter} from "@nestjs/platform-ws";
 
 async function bootstrap() {
 	const app = await NestFactory.create(AppModule);
-	// app.useWebSocketAdapter(new WildcardsIoAdapter(app));
-	await app.listen(6000);
+	app.useWebSocketAdapter(new WsAdapter(app));
+	await app.listen(7000);
 }
 
 bootstrap();
--- src/test.js
+++ src/test.js
@@ -2,6 +2,7 @@ const io = require('socket.io-client');
 
 console.log(1)
 const socket = io("http://localhost:6000")
+console.log(socket)
 console.log(2)
 socket.emit('test', {data: 'test'})
 console.log(3)
